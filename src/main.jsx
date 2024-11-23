907af933b335e1dc3e1635d09902107a4b950def
kratijun <oliver@krati.at>
2024-11-23 22:19:54 +0100

    just some fixes

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,15 @@ import Mobile from './Mobile.jsx';
 import QrCodePage from './Code.jsx';
 import RessourceManagement from './RessourceManagement.jsx';
 
-
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<App />} />
-                <Route path="Admin" element={<App />} />
-                <Route path="Mobile" element={<Mobile />} />
-                <Route path="generate" element={<QrCodePage />} />
-                <Route path="ressource-management" element={<RessourceManagement />} />
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter basename="/">
+        <Routes>
+            <Route index element={<App />} />
+            <Route path="Admin" element={<App />} />
+            <Route path="Mobile" element={<Mobile />} />
+            <Route path="generate" element={<QrCodePage />} />
+            <Route path="ressource-management" element={<RessourceManagement />} />
+        </Routes>
+    </BrowserRouter>
+
 );
